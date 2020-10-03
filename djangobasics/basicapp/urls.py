351a7c57f56d9eaff71b0a06d11aef4f2fae95f3
Dileshwori <dileshowri.joshi@sagarmatha.edu.np>
2020-10-03 12:22:05 +0545

    playing with django views and urls

--- djangobasics/basicapp/urls.py
+++ djangobasics/basicapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<question_id>/', views.detail, name='detail'),
+    #127.0.0.1/basicapp/2
+    path('<question_id>/results/', views.results, name='results'),
+    path('<question_id>/vote/', views.vote, name='vote'),
 ]
--- djangobasics/basicapp/views.py
+++ djangobasics/basicapp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Awesome job Dileshwori. this is your index page")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join(q.question_text for q in latest_questions)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("tHIS IS THE DETAIL VIEW OF THE QUESTION: %s" %question_id)
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question: %s" %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("VOTE on question: %s" %question_id)
