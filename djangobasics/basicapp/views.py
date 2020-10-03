bd487b7c3bb85428ecb34b9643b7507333a11364
Dileshwori <dileshowri.joshi@sagarmatha.edu.np>
2020-10-03 12:53:19 +0545

    connecting views and templates

--- djangobasics/basicapp/views.py
+++ djangobasics/basicapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader, RequestContext
 
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join(q.question_text for q in latest_questions)
-    return HttpResponse(output)
+    template = loader.get_template('basicapp/index.html')
+    context = {
+        'latest_questions': latest_questions
+     }
+    return render(request, 'basicapp/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("tHIS IS THE DETAIL VIEW OF THE QUESTION: %s" %question_id)
