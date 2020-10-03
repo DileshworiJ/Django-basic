0f467bf60feef4ff0a55d2cfafb34a6eb12009f8
Dileshwori <dileshowri.joshi@sagarmatha.edu.np>
2020-10-03 13:31:27 +0545

    Templating in Django

--- djangobasics/basicapp/views.py
+++ djangobasics/basicapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader, RequestContext
@@ -6,17 +6,17 @@ from django.template import loader, RequestContext
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('basicapp/index.html')
     context = {
         'latest_questions': latest_questions
      }
     return render(request, 'basicapp/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("tHIS IS THE DETAIL VIEW OF THE QUESTION: %s" %question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'basicapp/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse("These are the results of the question: %s" %question_id)
+    return HttpResponse("These are the results of the question: %s" % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("VOTE on question: %s" %question_id)
+    return HttpResponse("VOTE on question: %s" % question_id)
